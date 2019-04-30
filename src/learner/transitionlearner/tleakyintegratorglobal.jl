dbfbf18b89d8f3f07c3acbef954e51d678400967
VL <vasiliki.liakoni@epfl.ch>
2019-04-30 16:42:06 +0200

    TLeakyIntegrator: non-forgetting rest of T matrix (vs global leaky)

--- src/learner/transitionlearner/tleakyintegratorglobal.jl
+++ src/learner/transitionlearner/tleakyintegratorglobal.jl
@@ -25,13 +25,16 @@ end
 export TLeakyIntegrator
 """ X[t] = etaleak * X[t-1] + etaleak * I[.] """
 function updatet!(learnerT::TLeakyIntegrator, s0, a0, s1)
-    # ------ VERSION 2: Leave rest s0, a0 untouched
-    learnerT.Nsa[a0, s0] *= learnerT.etaleak # Discount transition
-    learnerT.Nsa[a0, s0] += learnerT.etaleak # Increase observed transition
-
-    for sprime in 1:learnerT.ns
-        learnerT.Ns1a0s0[sprime][(a0, s0)] *= learnerT.etaleak # Discount all outgoing transitions
+    # ------ VERSION 1: Leak the whole transition matrix
+    for s in 1:learnerT.ns
+        for a in 1:learnerT.na
+            learnerT.Nsa[a, s] *= learnerT.etaleak # Discount everything
+            for sprime in 1:learnerT.ns
+                learnerT.Ns1a0s0[sprime][(a, s)] *= learnerT.etaleak # Discount everything
+            end
+        end
     end
+    learnerT.Nsa[a0, s0] += learnerT.etaleak # Increase observed transition
     learnerT.Ns1a0s0[s1][(a0, s0)] += learnerT.etaleak # Increase observed transition
 end
 export updatet!
